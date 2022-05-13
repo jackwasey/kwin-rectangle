632bc7406375c715a9d7b3e3cc832a10658c251b
Jack O. Wasey <jack@jackwasey.com>
2022-05-13 14:11:35 -0400

    install to XDG user if possible

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/sh
+
+set -eu
 
 qdbus org.kde.KWin /Scripting org.kde.kwin.Scripting.unloadScript kwin-rectangle || true
 
@@ -6,14 +8,32 @@ plasmapkg2 -t kwinscript -r kwin-rectangle || true
 plasmapkg2 -t kwinscript -i .
 
 qdbus org.kde.KWin /Scripting org.kde.kwin.Scripting.loadScript \
-    ~/.local/share/kwin/scripts/kwin-rectangle kwin-rectangle
+  ~/.local/share/kwin/scripts/kwin-rectangle kwin-rectangle
 qdbus org.kde.KWin /KWin org.kde.KWin.reconfigure
 qdbus org.kde.KWin /Scripting org.kde.kwin.Scripting.start
 
-if [ ! -f "/usr/share/kservices5/kwin-rectangle.desktop" ]; then
-    echo "I will now create a symlink on a system folder."
-    echo "Without it you will not see the plugin's settings button."
-    echo "The plugin is already installed. If you do not mind the default settings, you can cancel"
-    echo "the symlink creation."
-    sudo ln -s "${PWD}/metadata.desktop" /usr/share/kservices5/kwin-rectangle.desktop
-fi
+src="${PWD}/metadata.desktop"
+for dst in \
+  "/usr/share/kservices5/kwin-rectangle.desktop" \
+  "${XDG_DATA_HOME}/kservices5/kwin-rectangle.desktop"; do
+
+  if [ -h "${dst}" ]; then
+    echo 2>&1 "${dst} was a symlink. Deleting."
+    rm -f "${dst}"
+  fi
+
+  if [ -e "${dst}" ]; then
+    #echo 2>&1 "${dst} already exists. Deleting."
+    rm -f "${dst}"
+  fi
+
+  if cp -f "${src}" "${dst}" 2>/dev/null; then
+    echo >&2 "Success: installed to ${dst}"
+  else
+    if sudo --non-interactive cp -f "${src}" "${dst}" 2>/dev/null; then
+      echo >&2 "Success: installed with sudo to ${dst}"
+    else
+      echo >&2 "Failed: $USER and sudo failed to install to ${dst}"
+    fi
+  fi
+done
